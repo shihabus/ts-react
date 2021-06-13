0df2e9998b3ae9841f3daf120294a3934bf4ec96
Shihabudheen US <shihabudheen@dunzo.in>
2021-06-13 12:58:18 +0530

    class component initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-function App() {
-  return <div className="App">Hello</div>;
-}
+import React, { Component } from "react";
 
-export default App;
+export default class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      count: props.defaultCount,
+    };
+  }
+  render() {
+    const { count }: any = this.state;
+    return (
+      <div>
+        <h1>Count {count}</h1>
+        <button
+          onClick={() => {
+            this.setState(({ count }: any) => ({ count: count - 1 }));
+          }}
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            this.setState(({ count }: any) => ({ count: count + 1 }));
+          }}
+        >
+          +
+        </button>
+      </div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import { render } from "react-dom";
 import App from "./App";
 import "./index.css";
 
-render(<App />, document.getElementById("root"));
+render(<App defaultCount={0} />, document.getElementById("root"));
